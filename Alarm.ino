805957a0c14a4dcbc9341f8503308781d2afb8af
David Walker <thewalker.david@gmail.com>
2022-08-26 18:07:15 -0400

    Created the arlarm code file

---
+++ Alarm.ino
@@ -0,0 +1,23 @@
+#include <pitches.h>
+#include <IRremote.h>
+#include <IRremoteInt.h>
+#include <SR04.h>
+#include <LiquidCrystal.h>
+
+#define TRIGPIN
+#define ECHOPIN
+
+const int rs = 7, e = 6, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
+LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
